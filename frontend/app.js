e28d71aabfdb643a85c469e651986471d3083d10
Patryk <patryk@simcraft.local>
2026-03-12 22:37:34 +0100

    Increase delay to 2000ms

--- frontend/app.js
+++ frontend/app.js
@@ -308,7 +308,7 @@ function app() {
             Alpine.nextTick(() => {
               Alpine.initTree(container);
             });
-          }, 1000);
+          }, 2000);
         }
       }
     },
